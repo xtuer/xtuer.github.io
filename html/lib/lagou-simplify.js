7b6a8324c89735a400675b58dfe853ef289ec1ae
Biao <huangbiao@shindata.com>
2020-11-05 17:42:40 +0800

    Site updated: 2020-11-05 17:42:39

---
+++ html/lib/lagou-simplify.js
@@ -0,0 +1,3 @@
+$('.right-content-wrap').css({ marginLeft: 0, width: '100%', marginTop: 0, top: 0, height: 'auto' });
+$('.right-content-wrap .main-wrap').css({ maxWidth: '100%', paddingLeft: '20px', paddingRight: '20px' });
+$('.pub-header, .pc-header, .left-content-wrap').remove();
